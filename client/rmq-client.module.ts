0f8378ee013cd0579087e8770ee569f180ffc25e
Miloš Danilović <danilovic.milos96@gmail.com>
2022-05-10 15:55:00 +0200

    RMQ Client - Async registration

--- client/rmq-client.module.ts
+++ client/rmq-client.module.ts
@@ -1,11 +1,14 @@
-import { DynamicModule, Module, OnApplicationShutdown } from '@nestjs/common';
+import { DynamicModule, Module, OnApplicationShutdown } from "@nestjs/common";
 import {
   ClientProxy,
   ClientsModuleOptions,
   ClientProviderOptions,
+  ClientsModuleAsyncOptions,
   Closeable,
-} from '@nestjs/microservices';
-import { RMQClientFactory } from './rmq-client-factory';
+} from "@nestjs/microservices";
+
+// Factory
+import { RMQClientFactory } from "./rmq-client-factory";
 
 @Module({})
 export class RMQClientModule {
@@ -22,6 +25,45 @@ export class RMQClientModule {
 
     return {
       module: RMQClientModule,
+      providers: clients,
+      exports: clients,
+    };
+  }
+
+  /**
+   * Registers the module asynchronously.
+   * @param options - Module options.
+   * @returns The registered module.
+   */
+  static async registerAsync(
+    options: ClientsModuleAsyncOptions
+  ): Promise<DynamicModule> {
+    let imports = [];
+    const clients = [];
+
+    for (const item of options || []) {
+      imports = [...imports, ...item.imports];
+
+      // Create the options for the provider.
+      const providerOptions = `${String(item.name)}_OPTIONS`;
+      clients.push({
+        provide: providerOptions,
+        useFactory: item.useFactory,
+        inject: item.inject,
+      });
+
+      // Create the provider based on the options.
+      clients.push({
+        provide: item.name,
+        useFactory: (options: ClientProviderOptions) =>
+          this.assignOnAppShutdownHook(RMQClientFactory.create(options)),
+        inject: [providerOptions],
+      });
+    }
+
+    return {
+      module: RMQClientModule,
+      imports: imports,
       providers: clients,
       exports: clients,
     };
